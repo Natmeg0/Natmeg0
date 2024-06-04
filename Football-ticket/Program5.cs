4181be0775151624af97d3589a32b4d849e34800
Natmeg0 <sv4519822@gmail.com>
2024-06-04 22:07:46 +0300

    Add files via upload

---
+++ Football-ticket/Program5.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp5;
+
+Stadium stadium = new Stadium(10000);
+
+if (stadium.BuyTicket())
+{
+    Console.WriteLine($"You bought a ticket. Tickets left:{stadium.GetAvailableTickets()}");
+}
+else
+{
+    Console.WriteLine("You can't buy a ticket. Tickets are out");
+}
+
+
+if (stadium.ReturnTicket())
+{
+    Console.WriteLine("You can't return a ticket.");
+}
+else
+{
+    Console.WriteLine($"Ticket returned. Tickets left: {stadium.GetAvailableTickets()}");
+}
---
+++ Football-ticket/Stadium.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class Stadium
+    {
+        private int _allTickets;
+        private int _soldTickets;
+
+        public Stadium(int totalTickets)
+        {
+            _allTickets = totalTickets;
+            _soldTickets = 0;
+        }
+        public bool BuyTicket()
+        {
+            if (_soldTickets < _allTickets)
+            {
+                _soldTickets += 1;
+                return true;
+            }
+            return false;
+        }
+
+        public bool ReturnTicket()
+        {
+            if (_soldTickets > 0)
+            {
+                _soldTickets -= 1;
+                return true;
+            }
+            return false;
+        }
+        public int GetAvailableTickets()
+        {
+            return _allTickets - _soldTickets;
+        }
+    }
+}
